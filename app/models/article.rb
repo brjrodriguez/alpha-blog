1d0d1be6728072b115fb850a660152036a3075e0
Bjrodriguez <bjrodriguez2021@gmail.com>
2021-07-31 11:00:13 -0400

    Adding articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  #this makes sure the data is validated
+  validates :title, presence: true, length: {minimum: 6 , maximum: 100}
+  validates :description, presence: true , length: {minimum: 10 , maximum: 300}
+
+
 end
